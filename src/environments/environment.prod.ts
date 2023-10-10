ec591340c2f7ccf9b9691da928f7fe19743d974e
DiCabrera <diegoc1739@gmail.com>
2023-10-10 01:31:45 -0300

    Implementación Firebase
    Se implementó firebase:
    -npm install firebase
    -firebase login
    -ng add @angular/fire

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyB9ClLBDPhzxAkOzKEWfZc309x2rXZTfX4",
+    authDomain: "proyectomobile002d.firebaseapp.com",
+    projectId: "proyectomobile002d",
+    storageBucket: "proyectomobile002d.appspot.com",
+    messagingSenderId: "22567357383",
+    appId: "1:22567357383:web:2f50445f46fcb4858ee7e2"
+    }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyB9ClLBDPhzxAkOzKEWfZc309x2rXZTfX4",
+    authDomain: "proyectomobile002d.firebaseapp.com",
+    projectId: "proyectomobile002d",
+    storageBucket: "proyectomobile002d.appspot.com",
+    messagingSenderId: "22567357383",
+    appId: "1:22567357383:web:2f50445f46fcb4858ee7e2"
+    }
 };
 
 /*
